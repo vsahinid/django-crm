fd7d2ac44634f67427304b8fd867d1730a356740
vsahinid <vsahinid@alumni.cmu.edu>
2021-03-01 12:03:47 -0600

    Updated url routes

--- accounts/models.py
+++ accounts/models.py
@@ -23,15 +23,15 @@ class Tag(models.Model):
 class Product(models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
-        ('Outdoor', 'Outdoor'),
+        ('Out Door', 'Out Door'),
     )
 
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField(null=True)
-    category = models.CharField(max_length=200, choices=CATEGORY)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
     description = models.CharField(max_length=200, null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-    tag = models.ManyToManyField(Tag)
+    tags = models.ManyToManyField(Tag)
 
     def __str__(self):
         return self.name
@@ -41,10 +41,14 @@ class Order(models.Model):
     STATUS = (
         ('Pending', 'Pending'),
         ('Out for delivery', 'Out for delivery'),
-        ('Delivery', 'Delivery'),
+        ('Delivered', 'Delivered'),
     )
+
     customer = models.ForeignKey(
         Customer, null=True, on_delete=models.SET_NULL)
     product = models.ForeignKey(Product, null=True, on_delete=models.SET_NULL)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
-    status = models.CharField(max_length=200, choices=STATUS)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
+
+    def __str__(self):
+        return self.product.name
