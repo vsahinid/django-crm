4be909be67fadc9a5c1de07cc3520f72a3cd8f2e
vsahinid <vsahinid@alumni.cmu.edu>
2021-02-28 20:51:48 -0600

    Added bootstrap and updated views

--- accounts/views.py
+++ accounts/views.py
@@ -3,12 +3,12 @@ from django.http import HttpResponse
 
 
 def home(request):
-    return HttpResponse('Home Page')
+    return render(request, 'accounts/dashboard.html')
 
 
 def products(request):
-    return HttpResponse('Products Page')
+    return render(request, 'accounts/products.html')
 
 
 def customer(request):
-    return HttpResponse('Products Page')
+    return render(request, 'accounts/customer.html')
